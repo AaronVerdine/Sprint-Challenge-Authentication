f8af9479c0058dd6a0ed18ceb762bc3e880fabc1
Your name <a.m.verdine@gmail.com>
2020-05-17 20:40:47 -0400

    Create and connect API

--- api/server.js
+++ api/server.js
@@ -1,18 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+const authenticate = require("../auth/authenticate-middleware.js");
+const authRouter = require("../auth/auth-router.js");
+const jokesRouter = require("../jokes/jokes-router.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(logger);
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/jokes", authenticate, jokesRouter);
+
+server.use("/", (req, res) => {
+  res.status(200).send(`<h1>Welcome</h1>`);
+});
+
+function logger(req, res, next) {
+  console.log(
+    `${new Date().toISOString()} ${req.ip} ${req.method} ${req.url} `
+  );
+
+  next();
+}
 
 module.exports = server;
