6d195f07ecb8d45101727d147b904e2f82bc22b5
Your name <a.m.verdine@gmail.com>
2020-05-17 20:58:04 -0400

    Write, connect, and test /register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,29 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+const Users = require("../auth/auth-model");
+
+router.post("/register", (req, res) => {
   // implement registration
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 12);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then((saved) => {
+      if (req.body) {
+        res.status(201).json(saved);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
